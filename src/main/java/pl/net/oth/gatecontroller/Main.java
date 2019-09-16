626c91168a2b7b618b0fa81eb4ed1068fad9e10f
Piotr Sokołowski <piotr.sokolowski@orange.com>
2019-09-16 19:44:45 +0200

    Pętla w main

--- src/main/java/pl/net/oth/gatecontroller/Main.java
+++ src/main/java/pl/net/oth/gatecontroller/Main.java
@@ -19,62 +19,65 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableAutoConfiguration
 public class Main {
 	private final static Log LOGGER = LogFactory.getLog(Main.class);
-	
+
 	@Autowired
 	private MqttCallback mqttCallback;
-	
+
 	private MqttClient client;
+
 	public static void main(String[] args) {
-		 SpringApplication.run(Main.class, args);
+		SpringApplication.run(Main.class, args);
 	}
+
 	public Main() {
-						
-		LOGGER.info("Application started");
-		try {				
-			
-			connect();
-			client.subscribe("telemetry/gate", 
-					(topic, msg)-> {
-						mqttCallback.messageArrived(topic, msg);
-						});	
-			client.setCallback(new MqttCallback() {
-				
-				@Override
-				public void messageArrived(String topic, MqttMessage message) throws Exception {
-					// TODO Auto-generated method stub
-					
-				}
-				
-				@Override
-				public void deliveryComplete(IMqttDeliveryToken token) {
-					// TODO Auto-generated method stub
-					
-				}
-				
-				@Override
-				public void connectionLost(Throwable cause) {
-					try {
-						connect();
-					} catch (MqttException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+		do {
+			LOGGER.info("Application restarted");
+			try {
+
+				connect();
+				client.subscribe("telemetry/gate", (topic, msg) -> {
+					mqttCallback.messageArrived(topic, msg);
+				});
+				client.setCallback(new MqttCallback() {
+
+					@Override
+					public void messageArrived(String topic, MqttMessage message) throws Exception {
+						// TODO Auto-generated method stub
+
 					}
-					
-				}
-			});
-			//client.set
-		} catch (MqttException e) {
-			LOGGER.error(e.getMessage());
-			e.printStackTrace();		
-		}
-	
+
+					@Override
+					public void deliveryComplete(IMqttDeliveryToken token) {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public void connectionLost(Throwable cause) {
+						try {
+							connect();
+						} catch (MqttException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+
+					}
+				});
+				// client.set
+			} catch (MqttException e) {
+				LOGGER.error(e.getMessage());
+				e.printStackTrace();
+			}
+		} while (true);
+
 	}
+
 	public void connect() throws MqttException {
 		client = new MqttClient("tcp://10.4.0.82:1883", MqttClient.generateClientId());
 		MqttConnectOptions options = new MqttConnectOptions();
 		options.setAutomaticReconnect(true);
 		options.setCleanSession(true);
-		options.setConnectionTimeout(10);			
+		options.setConnectionTimeout(10);
 		client.connect(options);
 	}
 }
